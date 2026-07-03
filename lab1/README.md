fda201c89ca8eda3bc98d8c62486c84e0b1ea88e
Be8Sen <suny10fb@gmail.com>
2026-07-03 14:11:54 +0530

    done with lab1

---
+++ lab1/README.md
@@ -0,0 +1,11 @@
+# LAB 1
+
+```sql
+SHOW DATABASES;
+```
+!(List of Databases)[screen_shots/show_databases.png]
+
+```sql
+DESCRIBE Customers;
+```
+!(Describing customers table)[screen_shots/describe_customers.png]
---
+++ lab1/lab1_queries.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE BankingDB;
+
+SHOW DATABASES;
+
+USE BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	Email VARCHAR(100),
+	Phone VARCHAR(15),
+	AccountCreationDate DATE
+);
+
+DESCRIBE Customers;
